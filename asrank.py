7bd153159d5348d0ba8aba6e005791077a2b9134
valedithat <valedithat@pm.me>
2023-03-25 00:42:11 -0400

    update to class

--- asrank.py
+++ asrank.py
@@ -1,16 +1,25 @@
 import requests
 import argparse
 
+class ASRank:
+    def __init__(self, args):
+        self.file = args.file
+        self.order = args.order
+        self.orgs_list = self.ingest_org_list()
 
-def ingest_org_list(args):
-    orgs_file = open(args.file, "r")
-    organizations = [org.strip().replace(' ', '') for org in orgs_file.readlines()]
-    print(f"Total Organizations: {len(organizations)}")
-    print(f"Sort Order: {args.order}")
-    return organizations
+        self.orgs = []
+        self.term_output = """
+    Rank\t\t Cone Size\t\t Organization Name
+    -----\t\t ----------\t\t -------------------"""
 
-def fetch_highest_ranking_asn(org):
-    try:
+    def ingest_org_list(self):
+        orgs_file = open(self.file, 'r')
+        organizations = [org.strip().replace(' ', '') for org in orgs_file.readlines()]
+        print(f"Total Organizations: {len(organizations)}")
+        print(f"Sort Order: {self.order}")
+        return organizations
+
+    def fetch_highest_ranking_asn(self, org):
         req = requests.get(f"https://api.asrank.caida.org/v2/restful/asns/name/{org}")
         asns = req.json()
 
@@ -25,68 +34,59 @@ def fetch_highest_ranking_asn(org):
             return org_id
         else:
             raise ValueError(f"No ASName found for {org}")
-    except ValueError as e:
-        print(e)
 
-def fetch_organization(org_id):
-    req = requests.get(f"https://api.asrank.caida.org/v2/restful/organizations/{org_id}")
-    orgs = req.json()
+    def fetch_organization(self, org_id):
+        req = requests.get(f"https://api.asrank.caida.org/v2/restful/organizations/{org_id}")
+        orgs = req.json()
 
-    if orgs['data']['organization']:
-        org = orgs['data']['organization']
-        org_details = {
-            'name': org['orgName'],
-            'rank': org['rank'],
-            'cone': org['cone']['numberAsns'],
-        }
-        return org_details
+        if orgs['data']['organization']:
+            org = orgs['data']['organization']
+            org_details = {
+                'name': org['orgName'],
+                'rank': org['rank'],
+                'cone': org['cone']['numberAsns'],
+            }
+            return org_details
 
-def sort_orgs(orgs, order):
-    for i in range(len(orgs)):
-        swapped = False
+    def sort_orgs(self):
+        for i in range(len(self.orgs)):
+            swapped = False
 
-        for j in range(0, len(orgs) - i - 1):
-            order = 'rank' if order == 'rank' else 'cone'
+            for j in range(0, len(self.orgs) - i - 1):
+                order = 'rank' if self.order == 'rank' else 'cone'
 
-            if orgs[j][order] < orgs[j+1][order]:
-                orgs[j], orgs[j+1] = orgs[j+1], orgs[j]
-                swapped = True
+                if self.orgs[j][order] < self.orgs[j+1][order]:
+                    self.orgs[j], self.orgs[j+1] = self.orgs[j+1], self.orgs[j]
+                    swapped = True
 
-        if(swapped == False):
-            break
+            if(swapped == False):
+                break
 
-def present_orgs(orgs):
-    result = """
-Rank\t\t Cone Size\t\t Organization Name
------\t\t ----------\t\t -------------------"""
+    def present_orgs(self):
+        for org in self.orgs:
+            self.term_output += f"""
+{org['rank']}\t\t\t {org['cone']}\t\t\t {org['name']}"""
 
-    for org in orgs:
-        result += f"""
-{org['rank']}\t\t {org['cone']}\t\t\t {org['name']}"""
+        print(self.term_output)
 
-    print(result)
+    def rank(self):
+        for org in self.orgs_list:
+            try:
+                org_id = self.fetch_highest_ranking_asn(org)
 
-def query_orgs(orgs_list, args):
-    orgs = []
-    for org in orgs_list:
-        try:
-            org_id = fetch_highest_ranking_asn(org)
+                if org_id:
+                    org_details = self.fetch_organization(org_id)
+                    self.orgs.append(org_details)
+            except ValueError as e:
+                print(e)
 
-            if org_id:
-                org_details = fetch_organization(org_id)
-                orgs.append(org_details)
-        except ValueError as e:
-            print(e)
-
-    if orgs:
-        sort_orgs(orgs, args)
-        present_orgs(orgs)
-
+        if self.orgs:
+            self.sort_orgs()
+            self.present_orgs()
 
 parser = argparse.ArgumentParser(prog='ASRank')
 parser.add_argument('file', help='org list')
-parser.add_argument('--order', choices=['rank', 'cone'], help='rank or cone size')
-
+parser.add_argument('order', choices=['rank', 'cone'], help='ig')
 args = parser.parse_args()
-orgs = ingest_org_list(args)
-query_orgs(orgs, args)
+asrank = ASRank(args)
+asrank.rank()
